5e508b33c56eb35280d26c3d746b73a41a60fa5b
Your Name <you@example.com>
2026-03-25 16:47:26 +0100

    Add fork contribution

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -131,7 +131,7 @@ class Library {
         }
     }
 }
-
+// Updated by 243033
 public class SI2026Lab1Main {
     public static void main(String[] args) {
         Library library = new Library();
