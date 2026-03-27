03d47526155d97c51bcede61aa3d8665c8ad575c
Anastasija Stojkova <stejza@Anastasijas-MacBook-Air.local>
2026-03-28 00:09:42 +0100

    Add fork contribution

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -134,6 +134,7 @@ class Library {
 
 public class SI2026Lab1Main {
     public static void main(String[] args) {
+	//Updated by 246009
         Library library = new Library();
 
         library.addBook(new Book("Clean Code", "Robert C. Martin", "Programming"));
