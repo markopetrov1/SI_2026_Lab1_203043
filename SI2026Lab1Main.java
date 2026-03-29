4de7b6cd9ed911dc2932119ab48c251dd4a04707
sxCBR1KRR <milos.pajkoski@yahoo.com>
2026-03-29 19:31:51 +0200

    Fix comment format

--- SI2026Lab1Main.java
+++ SI2026Lab1Main.java
@@ -133,7 +133,7 @@ class Library {
 }
 
 public class SI2026Lab1Main {
-    // update by 226129 :)
+    // Updated by 226129
     public static void main(String[] args) {
         Library library = new Library();
 
